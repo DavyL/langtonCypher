45295cfce7c6f754c359b2ff265a0a09c34022b4
DavyL <cr4zypxl@gmail.com>
2018-05-25 17:13:55 +0200

    EOD

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #define HEIGHT 100
 #define WIDTH 100
@@ -16,10 +17,19 @@ struct latticeStruct {
 };
 
 void * printBasics(struct antStruct *, struct latticeStruct *);
+void * displayGrid(struct latticeStruct * lattice);
 void * direction(struct antStruct * ant, struct latticeStruct * lattice);
 void * paint(struct antStruct *, struct latticeStruct *);
 void * move( struct antStruct *, struct latticeStruct *);
 int successor( struct antStruct *, struct latticeStruct *, int );
+
+
+long long decimalToBinary(long long  );
+long long binaryToDecimal(long long  );
+
+int * numberToArray(long long);
+  
+int * textToBinArray( char *);
 
 int mod(int, int);
 
@@ -44,12 +54,31 @@ int main( int argc, char ** argv, char **envv){
 	ant.y = 0;
 	ant.dir = 3;					//The ant starts at the origin, pointing towards the top
 	
-	successor(&ant, &lattice, 10000);
+	char * sentence = "Hello World";	
+	int numSentence = (int)(sentence);
+	printf("numsentence = %d\n", numSentence);
+	numSentence = decimalToBinary( numSentence);
+	printf("numsentence = %d\n", numSentence);
+	
+
 }
 
 void * printBasics(struct antStruct * ant, struct latticeStruct * lattice){
 	printf("Ant is at location : %d, %d, pointing towards : %d, on a case of color : %d\n", ant->x, ant->y, ant->dir, lattice->grid[ant->x][ant->y]);
 }
+
+void * displayGrid(struct latticeStruct * lattice){
+	int i = 0;
+	int j= 0;
+
+	for( i = 0; i < lattice->height; i++){
+		for( j = 0; j < lattice->width; j++){
+			printf("%d",lattice->grid[i][j]);
+		}
+	printf("\n");
+	}
+}
+
 void * direction(struct antStruct * ant, struct latticeStruct * lattice){	//Changes the color of the case on which the ant steps
 	/*if (lattice->grid[ant->x][ant->y] == -1)
 		ant->dir = mod(ant->dir - 1, 4);
@@ -59,6 +88,7 @@ void * direction(struct antStruct * ant, struct latticeStruct * lattice){	//Chan
 	ant->dir = mod(ant->dir + lattice->grid[ant->x][ant->y], 4); 	//This is a shorter version of the above
 									//The above is left in order to make code review easier :)
 }
+
 void * paint(struct antStruct * ant, struct latticeStruct * lattice){
 	lattice->grid[ant->x][ant->y] = -lattice->grid[ant->x][ant->y];	//Changing the color of a case corresponds to multiplying it by -1
 }
@@ -89,6 +119,47 @@ int successor( struct antStruct * ant, struct latticeStruct * lattice, int n ){	
 		paint(ant, lattice);
 	}
 	return n;
+}
+
+long long decimalToBinary(long long decimal){
+	int i = 0;
+	long long binary = 0;
+	int remainder = 0;
+	//int step = 0;
+	for( i = 1; decimal != 0; i *= 10){
+	        remainder = decimal%2;
+            	decimal /= 2;
+        	binary += i*remainder;
+    	}				
+	return binary;
+}
+
+
+
+long long binaryToDecimal(long long binary){
+	int i = 0;
+	long long decimal = 0;
+	int remainder = 0;
+	for( i =  1; binary != 0; i*= 2){
+		remainder = binary%10;
+		binary /= 10;
+		decimal += i*remainder;
+	}
+	return decimal;
+}
+
+int * numberToArray(long long number){
+	int i = 0;
+	int length = (int) (log10((float) number)) + 1;
+	int * array = (int *) malloc(length * sizeof(int));
+	for(i = length; i != 0 ; number /= 10, --i){
+		array[i] = number%10;
+	}
+	return array;
+}
+
+int * textToBinArray( char * text){
+	return numberToArray(decimalToBinary((int) text ));
 }
 
 int mod(int a, int b){
